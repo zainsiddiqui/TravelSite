74f1f0f6cfe87417c06d9265dc6a88734eb366ab
Zain <zainsid11@gmail.com>
2019-12-05 16:12:25 -0500

    commit flights add

--- TravelSiteV2/src/FlightsAdd.java
+++ TravelSiteV2/src/FlightsAdd.java
@@ -36,6 +36,7 @@ public class FlightsAdd extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		//hello
 	}
 
 }
